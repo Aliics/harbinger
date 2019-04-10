65b42b7dfe012249319fe7e9f65f28cbfae22e91
Alexander Johnston <Aliics@hotmail.com>
2019-04-11 10:39:01 +1200

    Created arg to option validation.
    Updated the arg to option dictionary creation function.
    
    Created function to check if an option requires a param.

--- arghandler.py
+++ arghandler.py
@@ -1,5 +1,39 @@
 import sys
 
-def arg_options_dict(arg):
-	return []
+valid_options = [
+	{
+		"option": "--help",
+		"requires_param": False,
+	},
+	{
+		"option": "--version",
+		"requires_param": False
+	},
+	{
+		"option": "--add",
+		"requires_param": True
+	}
+]
 
+def arg_options_dict(args):
+	options = []
+	for i in range(0, len(args)):
+		option = args[i]
+		if option_is_valid(option):
+			param = args[i + 1] if len(args) > i + 1 else ""
+			options.append({"option": option})
+			if not param == "" and option_req_param(option):
+				options[len(options) - 1]["param"] = param
+	return options
+
+def option_is_valid(option):
+	for valid_option in valid_options:
+		if option == valid_option["option"]:
+			return True
+	return False
+
+def option_req_param(option):
+	for valid_option in valid_options:
+		if option == valid_option["option"]:
+			return valid_option["requires_param"]
+	return False
--- test/test_arghandler.py
+++ test/test_arghandler.py
@@ -5,6 +5,7 @@ sys.path.append(".") # only works if run from the dir above
 
 import arghandler
 
+# arghandler.arg_option_dict
 def test_should_return_blank_dict_if_given_no_valid_args():
 	assert arghandler.arg_options_dict([]) == []
 
@@ -13,8 +14,50 @@ def test_should_return_two_valid_options_when_two_valid_args_are_given():
 	expected_options = [
 		{
 			"option": "--help",
-			"param": None
+		},
+		{
+			"option": "--version",
 		}
 	]
 	assert arghandler.arg_options_dict(args) == expected_options
+
+def test_should_return_one_valid_option_with_param_when_given_add_and_param():
+	args = ["--add", "foo"]
+	expected_options = [
+		{
+			"option": "--add",
+			"param": "foo"
+		}
+	]
+	assert arghandler.arg_options_dict(args) == expected_options
+
+def test_should_return_one_valid_option_with_param_when_given_only_one_valid_arg_and_param():
+	args = ["--foo", "--add", "foo", "--bar"]
+	expected_options = [
+		{
+			"option": "--add",
+			"param": "foo"
+		}
+	]
+	assert arghandler.arg_options_dict(args) == expected_options
+
+# arghandler.option_is_valid
+def test_should_return_true_when_given_version():
+	assert arghandler.option_is_valid("--version")
+
+def test_should_return_false_when_given_foo():
+	assert not arghandler.option_is_valid("--foo")
+
+def test_should_return_false_when_given_malformed_but_close_to_version():
+	assert not arghandler.option_is_valid("-version-")
+
+def test_should_return_false_when_given_blank():
+	assert not arghandler.option_is_valid("")
+
+# arghandler.option_req_param
+def test_should_return_true_when_given_add():
+	assert arghandler.option_req_param("--add")
+
+def test_should_return_false_when_given_version():
+	assert not arghandler.option_req_param("--version")
 
