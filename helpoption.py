fcbf55371277d59e08eee3635b3cddd8de3005e2
Alexander Johnston <Aliics@hotmail.com>
2019-04-11 15:48:00 +1200

    Made slightly nicer formating for the help command.

--- helpoption.py
+++ helpoption.py
@@ -1,13 +1,21 @@
 import harbinger
+import shutil
 
-info_text = "Harbinger is a docker orchestration tool power by a snakey boy."
+info_text = "Harbinger is a docker orchestration tool powered by a snakey boy."
 
 def format_help_text():
+	terminal_width = shutil.get_terminal_size().columns
 	help_text = "{:<10} | {}\n".format("option", "desc")
-	help_text += "=================================================================================================================================================\n"
+	help_text += draw_line(terminal_width)
 	for option in harbinger.OPTIONS:
 		help_text += "{:<10} | {}\n".format(option["option"], option["desc"])
 	return help_text
+
+def draw_line(width, character = "="):
+	line = ""
+	for i in range(0, width):
+		line += character
+	return line
 
 def call(param):
 	try :
--- test/test_helpoption.py
+++ test/test_helpoption.py
@@ -1,4 +1,5 @@
 import sys
+import shutil
 
 sys.path.append(".")
 
@@ -7,11 +8,20 @@ import helpoption
 
 # helpoption.format_help_text
 def test_should_return_the_exact_expected_string():
+	terminal_width = shutil.get_terminal_size().columns
 	expected = "option     | desc\n"
-	expected += "=================================================================================================================================================\n"
+	expected += helpoption.draw_line(terminal_width)
 	expected += "--help     | Displays information about harbinger and it's commands.\n"
 	expected += "--version  | Outputs the current version of harbinger.\n"
 	expected += "--list     | Outputs a list of docker containers and images that are either currently up, or can be brought up.\n"
 	expected += "--add      | Add an image to your cluster. Making it available to be run in as a container.\n"
 	assert helpoption.format_help_text() == expected
+
+
+# helpoption.draw_line
+def test_should_return_five_equal_characters():
+	assert helpoption.draw_line(5) == "====="
+
+def test_should_return_two_plus_characters():
+	assert helpoption.draw_line(2, "+") == "++"
 
