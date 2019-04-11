75a1eb1dad45fb3bab6bee59cf004ef89998a8b6
Alexander Johnston <Aliics@hotmail.com>
2019-04-12 09:53:26 +1200

    Created an image validation check function in addoption.

--- addoption.py
+++ addoption.py
@@ -1,2 +1,11 @@
+import os
+import listoption
+
+def image_exists(image_name, images): # could gather the images internally, but this is more modular
+	for image in images:
+		if image["image"] == image_name:
+			return True
+	return False
+
 def call(param):
 	return True
---
+++ test/test_addoption.py
@@ -0,0 +1,72 @@
+import os
+import sys
+
+sys.path.append(".")
+
+import pytest
+import addoption
+import listoption
+
+#addoption.image_exists
+def test_should_return_true_when_given_the_only_valid_docker_image():
+	docker_output = ["image version id mod time time size"]
+	images = listoption.format_docker_images_list(docker_output)
+	assert addoption.image_exists("image", images)
+
+def test_should_return_true_even_given_a_big_list_valid_docker_images():
+	docker_output = [
+		"image0 version id mod time time size",
+		"image1 version id mod time time size",
+		"image2 version id mod time time size",
+		"image3 version id mod time time size",
+		"image4 version id mod time time size",
+		"image5 version id mod time time size",
+		"image6 version id mod time time size",
+		"image7 version id mod time time size",
+		"image8 version id mod time time size",
+		"image9 version id mod time time size",
+		"image10 version id mod time time size",
+		"image11 version id mod time time size",
+		"image12 version id mod time time size",
+		"image13 version id mod time time size",
+		"image14 version id mod time time size",
+		"image15 version id mod time time size",
+		"image16 version id mod time time size",
+		"image17 version id mod time time size",
+		"image18 version id mod time time size",
+		"image19 version id mod time time size"
+	]
+	images = listoption.format_docker_images_list(docker_output)
+	assert addoption.image_exists("image7", images)
+
+def test_should_false_when_no_image_is_found_even_from_a_big_list():
+	docker_output = [
+		"image0 version id mod time time size",
+		"image1 version id mod time time size",
+		"image2 version id mod time time size",
+		"image3 version id mod time time size",
+		"image4 version id mod time time size",
+		"image5 version id mod time time size",
+		"image6 version id mod time time size",
+		"image7 version id mod time time size",
+		"image8 version id mod time time size",
+		"image9 version id mod time time size",
+		"image10 version id mod time time size",
+		"image11 version id mod time time size",
+		"image12 version id mod time time size",
+		"image13 version id mod time time size",
+		"image14 version id mod time time size",
+		"image15 version id mod time time size",
+		"image16 version id mod time time size",
+		"image17 version id mod time time size",
+		"image18 version id mod time time size",
+		"image19 version id mod time time size"
+	]
+	images = listoption.format_docker_images_list(docker_output)
+	assert not addoption.image_exists("image20", images)
+
+def test_should_return_false_when_search_is_over_an_empty_list():
+	docker_output = []
+	images = listoption.format_docker_images_list(docker_output)
+	assert not addoption.image_exists("image", images)
+
