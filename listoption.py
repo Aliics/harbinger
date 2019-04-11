81f5bc5e9148eef830fed41993ad661b1a49a3a2
Alexander Johnston <Aliics@hotmail.com>
2019-04-11 17:19:40 +1200

    Created a image name shortener function.

--- listoption.py
+++ listoption.py
@@ -4,17 +4,22 @@ def format_docker_images_list(docker_output):
 	image_list = []
 	for output in docker_output:
 		fields = output.split()
-		if (len(fields) > 4):
+		if (len(fields) == 7):
 			image_list.append(
 				{
-					"image": fields[0],
+					"image": shorten_image_name(fields[0]),
 					"version": fields[1],
 					"id": fields[2],
-					"mod_time": fields[3],
-					"size": fields[4]
+					"mod_time": "{} {} {}".format(fields[3], fields[4], fields[5]),
+					"size": fields[6]
 				}
 			)
 	return image_list
+
+def shorten_image_name(long_name):
+	name_dirs = long_name.split("/")
+	last_index = len(name_dirs) - 1 if len(name_dirs) > 0 else 0
+	return name_dirs[last_index]
 
 def call(param):
 	try:
--- test/test_listoption.py
+++ test/test_listoption.py
@@ -7,35 +7,47 @@ import listoption
 
 # listoption.docker_image_list
 def test_should_return_two_dicts_of_docker_image_info_when_given_two_valid_strings():
-	docker_output = ["foo bar wasd qwerty yes", "cake tastes really good man"]
+	docker_output = ["foo bar wasd qwerty weeks ago yes", "cake tastes really good weeks ago man"]
 	expected = [
 		{
 			"image": "foo",
 			"version": "bar",
 			"id": "wasd",
-			"mod_time": "qwerty",
+			"mod_time": "qwerty weeks ago",
 			"size": "yes"
 		},
 		{
 			"image": "cake",
 			"version": "tastes",
 			"id": "really",
-			"mod_time": "good",
+			"mod_time": "good weeks ago",
 			"size": "man"
 		}
 	]
 	assert listoption.format_docker_images_list(docker_output) == expected
 
 def test_should_return_one_dict_of_docker_image_info_when_given_one_invalid_string():
-	docker_output = ["foo bar wasd qwerty", "cake tastes really good man"]
+	docker_output = ["foo bar wasd qwerty", "cake tastes really good years ago man"]
 	expected = [
 		{
 			"image": "cake",
 			"version": "tastes",
 			"id": "really",
-			"mod_time": "good",
+			"mod_time": "good years ago",
 			"size": "man"
 		}
 	]
 	assert listoption.format_docker_images_list(docker_output) == expected
 
+# listoption.shorten_image_name
+def test_should_return_foo_when_given_ecr_url_with_image_name_foo():
+	ecr_url = "825119612905.dkr.ecr.eu-west-1.amazonaws.com/foo"
+	assert listoption.shorten_image_name(ecr_url) == "foo"
+
+def test_should_return_blank_when_given_an_empty_string():
+	blank_string = ""
+	assert listoption.shorten_image_name(blank_string) == ""
+
+def test_should_return_what_was_given_when_given_a_plain_string():
+	plain_string = "funname"
+	assert listoption.shorten_image_name(plain_string) == "funname"
