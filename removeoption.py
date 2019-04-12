f80d0afc29734853ee38c0a9454b3b8a7dd54d24
Alexander Johnston <Aliics@hotmail.com>
2019-04-12 14:59:29 +1200

    Created function for removing images that have been added.

--- removeoption.py
+++ removeoption.py
@@ -11,5 +11,19 @@ def image_is_added(image_name, file_name):
 		return False
 	return False
 
+def remove_added_image(image_name, file_name):
+	if image_is_added(image_name, file_name):
+		added_images = open(file_name, "r")
+		images = added_images.readlines()
+		added_images.close()
+		new_images = []
+		for image in images:
+			if not image_name == image.rstrip():
+				new_images.append(image)
+		added_images = open(file_name, "w")
+		added_images.writelines(new_images)
+		added_images.close()
+
 def call(param):
 	return True
+
--- test/test_removeoption.py
+++ test/test_removeoption.py
@@ -45,3 +45,43 @@ def test_should_return_false_when_given_an_empty_file():
 def test_should_return_false_when_given_a_non_existant_file():
 	assert not removeoption.image_is_added("foo", ".added_images")
 
+# removeoption.remove_added_image
+def test_should_remove_image_from_file_when_given_valid_image():
+	added_images = open(".added_images", "w")
+	added_images.write("foobar\n")
+	added_images.close()
+	removeoption.remove_added_image("foobar", ".added_images")
+	added_images = open(".added_images", "r")
+	currently_added = added_images.readlines()
+	added_images.close()
+	assert len(currently_added) == 0
+	os.remove(".added_images")
+
+def test_should_not_remove_anything_when_given_an_image_that_is_not_in_file():
+	added_images = open(".added_images", "w")
+	added_images.write("foo\n")
+	added_images.close()
+	removeoption.remove_added_image("bar", ".added_images")
+	added_images = open(".added_images", "r")
+	currently_added = added_images.readlines()
+	added_images.close()
+	assert len(currently_added) == 1
+	os.remove(".added_images")
+
+def test_should_only_remove_one_image_from_file_in_a_file_of_many():
+	added_images = open(".added_images", "w")
+	added_images.write("foo\n")
+	added_images.write("bar\n")
+	added_images.write("fizz\n")
+	added_images.write("buzz\n")
+	added_images.write("charles\n")						
+	added_images.close()
+	removeoption.remove_added_image("bar", ".added_images")
+	added_images = open(".added_images", "r")
+	currently_added = added_images.readlines()
+	added_images.close()
+	assert len(currently_added) == 4
+	os.remove(".added_images")
+
+
+
