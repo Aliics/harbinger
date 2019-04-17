93268bb7d5f5465b67026a40501d37fd37d9d6c8
Alexander Johnston <Aliics@hotmail.com>
2019-04-17 15:00:00 +1200

    Created basic up option functionality.

--- upoption.py
+++ upoption.py
@@ -1,3 +1,6 @@
+import os
+
+
 def list_added_images(file_name):
     try:
         added_images = open(file_name, "r")
@@ -11,5 +14,22 @@ def list_added_images(file_name):
         return []
 
 
+def run_docker_container(image_name):
+    try:
+        os.system("docker run {}".format(image_name))
+        return True
+    except:
+        return False
+
+
 def call(param):
-    return True
+    try:
+        added_images = list_added_images(".added_images")
+        if len(added_images) == 0:
+            print("Nice try! There are no containers to bring up. ;^)")
+            return True
+        for added_image in added_images:
+            run_docker_container(added_image)
+        return True
+    except:
+        return False
