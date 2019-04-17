f3ead2d04292848e5cd9521dc4183c0972233026
Alexander Johnston <Aliics@hotmail.com>
2019-04-17 16:51:03 +1200

    Created some nice functionality to down option.

--- downoption.py
+++ downoption.py
@@ -6,29 +6,37 @@ from multiprocessing import Process
 def list_container_ids(added_images, current_containers):
     container_ids = []
     for current_container in current_containers:
-        id = current_container.split()
-        container_ids.append(id[0])
+        current_details = current_container.split()
+        if len(current_details) > 6:
+            container_id = current_details[0]
+            container_name = current_details[1]
+            for added_image in added_images:
+                if container_name == added_image:
+                    container_ids.append(container_id)
+                    break
     return container_ids
 
 
-def stop_docker_container(image_name):
-    os.popen("docker stop {}".format(image_name)).close()
+def stop_docker_container(container_id):
+    os.system("docker stop {}".format(container_id))
 
 
 def call(param):
     try:
         added_images = upoption.list_added_images(".added_images")
-        current_containers = list_container_ids(added_images)
+        current_containers = list_container_ids(added_images, os.popen("docker ps").readlines())
         if len(current_containers) == 0:
             print("Nice try! There are no containers even in the cluster. ;^)")
             return True
+        print("Containers stopped:")
         processes = []
         for current_container in current_containers:
             docker_runner = Process(target=stop_docker_container, args=(current_container,))
             docker_runner.start()
-            processes.append(docker_runner)
-        for process in processes:
+            processes.append({current_container, docker_runner})
+        for container_id, process in processes:
             process.join()
+
         return True
     except:
         print("Uh oh! Something happened when bringing down a container. :^(")
--- test/test_downoption.py
+++ test/test_downoption.py
@@ -12,3 +12,56 @@ def test_should_return_expected_container_id_when_given_image_name_that_matches_
     containers = downoption.list_container_ids(["foo"], current_containers)
     expected = ["f395d8187d40"]
     assert containers == expected
+
+
+def test_should_return_multi_container_ids_when_image_has_multiple_instances_up():
+    current_containers = [
+        "f395d8187d40 foo \"/bin/sh -c ./run.sh\" 31 minutes ago Up 31 minutes 25735/tcp sharp_kalam",
+        "f395d8187d41 foo \"/bin/sh -c ./run.sh\" 32 minutes ago Up 32 minutes 25735/tcp sharp_kalam"
+    ]
+    containers = downoption.list_container_ids(["foo"], current_containers)
+    expected = ["f395d8187d40", "f395d8187d41"]
+    assert containers == expected
+
+
+def test_should_return_multi_container_ids_when_multiple_images_have_multiple_instances_up():
+    current_containers = [
+        "f395d8187d40 foo \"/bin/sh -c ./run.sh\" 31 minutes ago Up 31 minutes 25735/tcp sharp_kalam",
+        "f395d8187d41 foo \"/bin/sh -c ./run.sh\" 32 minutes ago Up 32 minutes 25735/tcp sharp_kalam",
+        "f395d8187d42 foobar \"/bin/sh -c ./run.sh\" 32 minutes ago Up 32 minutes 25735/tcp sharp_kalam",
+        "f395d8187d43 foobar \"/bin/sh -c ./run.sh\" 32 minutes ago Up 32 minutes 25735/tcp sharp_kalam"
+    ]
+    containers = downoption.list_container_ids(["foo", "foobar"], current_containers)
+    expected = ["f395d8187d40", "f395d8187d41", "f395d8187d42", "f395d8187d43"]
+    assert containers == expected
+
+
+def test_should_return_blank_when_desired_image_is_not_in_cluster():
+    current_containers = [
+        "f395d8187d40 foo \"/bin/sh -c ./run.sh\" 31 minutes ago Up 31 minutes 25735/tcp sharp_kalam",
+        "f395d8187d41 foo \"/bin/sh -c ./run.sh\" 32 minutes ago Up 32 minutes 25735/tcp sharp_kalam",
+        "f395d8187d42 foobar \"/bin/sh -c ./run.sh\" 32 minutes ago Up 32 minutes 25735/tcp sharp_kalam",
+        "f395d8187d43 foobar \"/bin/sh -c ./run.sh\" 32 minutes ago Up 32 minutes 25735/tcp sharp_kalam"
+    ]
+    containers = downoption.list_container_ids(["fizz"], current_containers)
+    expected = []
+    assert containers == expected
+
+
+def test_should_return_only_one_set_of_container_ids_even_when_multiple_exist():
+    current_containers = [
+        "f395d8187d40 foo \"/bin/sh -c ./run.sh\" 31 minutes ago Up 31 minutes 25735/tcp sharp_kalam",
+        "f395d8187d41 foo \"/bin/sh -c ./run.sh\" 32 minutes ago Up 32 minutes 25735/tcp sharp_kalam",
+        "f395d8187d42 foobar \"/bin/sh -c ./run.sh\" 32 minutes ago Up 32 minutes 25735/tcp sharp_kalam",
+        "f395d8187d43 foobar \"/bin/sh -c ./run.sh\" 32 minutes ago Up 32 minutes 25735/tcp sharp_kalam"
+    ]
+    containers = downoption.list_container_ids(["foobar"], current_containers)
+    expected = ["f395d8187d42", "f395d8187d43"]
+    assert containers == expected
+
+
+def test_should_return_blank_when_no_containers_are_up():
+    current_containers = []
+    containers = downoption.list_container_ids(["foo"], current_containers)
+    expected = []
+    assert containers == expected
